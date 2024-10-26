af96ae6e21f44497bd5fc7cca935c485cebfee45
anvrich <anvrich7000@gmail.com>
2024-10-26 12:30:18 +0300

    :rocket: #26 Remove Duplicates from Sorted Array

---
+++ easy/RemoveDuplicatesfromSortedArray_26/removeDuplicatesfromSortedArray.go
@@ -0,0 +1,17 @@
+package main
+
+func RemoveDuplicates(nums []int) int {
+
+	if len(nums) == 0 {
+		return 0
+	}
+
+	uniqueCount := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[uniqueCount] = nums[i]
+			uniqueCount++
+		}
+	}
+	return uniqueCount
+}
